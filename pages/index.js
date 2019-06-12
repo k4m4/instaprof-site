659c84e65470baadc98945b1cdf4f8e7260c201b
k4m4 <nikolaskam@gmail.com>
2019-06-13 02:52:03 +0300

    Fix minor typo in pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -77,7 +77,7 @@ class Index extends PureComponent {
         <section>
           <h1>InstaProf</h1>
 
-          <p>Enter an Instagram account username to view it's profile picture
+          <p>Enter an Instagram account username to view its profile picture
           in <i>full size</i>.</p>
 
           <form onSubmit={this.handleSubmit}>
